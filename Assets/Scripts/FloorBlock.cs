84b68918cbe54c8ca473a86bf0d524da162427d4
Talgat <ttokayev7@gmail.com>
2024-02-11 01:22:29 +0600

    Make Instantiate of bomb

--- Assets/Scripts/FloorBlock.cs
+++ Assets/Scripts/FloorBlock.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class FloorBlock : MonoBehaviour
 {
+    [SerializeField] private Bomb bombPrefab;
+    [SerializeField] private Transform floorBlockTopPoint;
+    
     public void Plant() {
         Debug.Log("Plant");
+        Bomb bomb = Instantiate(bombPrefab, floorBlockTopPoint);
+        bomb.transform.localPosition = Vector3.zero;
     }
 }
