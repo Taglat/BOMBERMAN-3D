dcc3d7567117eaef5de451d459bba0933cdb0071
Talgat <ttokayev7@gmail.com>
2024-02-11 16:23:46 +0600

    add to floor checker bomb

--- Assets/Scripts/FloorBlock.cs
+++ Assets/Scripts/FloorBlock.cs
@@ -6,10 +6,18 @@ public class FloorBlock : MonoBehaviour
 {
     [SerializeField] private Bomb bombPrefab;
     [SerializeField] private Transform floorBlockTopPoint;
-    
-    public void Plant() {
+
+    private Bomb currentBomb = null;
+    public void Plant()
+    {
+        if (currentBomb != null)
+        {
+            Debug.Log("There is already a bomb on this block.");
+            return;
+        }
+
         Debug.Log("Plant");
-        Bomb bomb = Instantiate(bombPrefab, floorBlockTopPoint);
-        bomb.transform.localPosition = Vector3.zero;
+        currentBomb = Instantiate(bombPrefab, floorBlockTopPoint);
+        currentBomb.transform.localPosition = Vector3.zero;
     }
 }
